03c9bf06025094270a63cfdd5d7b300c97a5f3ae
raberin <rmvaberin@gmail.com>
2019-05-27 21:47:06 -0700

    First Commit - Added Get endpoint

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
+
+const db = knex(knexConfig.development);
+
+const helmet = require("helmet");
 
 const server = express();
 
@@ -7,6 +12,17 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+//SELECT * from roles
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(animals => {
+      res.status(200).json(animals);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
